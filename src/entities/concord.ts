8cfc6dcb9a7a4c54f3433744f24c389a0028b6d5
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-03-25 15:44:39 +0200

    Define Concord

---
+++ src/entities/concord.ts
@@ -0,0 +1,59 @@
+import Entity from "./entity";
+import Duration from "./duration";
+import Note from "./note";
+import Pitch from "./pitch";
+
+/** @public */
+namespace Concord {
+	export interface GetCodeParams extends Entity.GetCodeParams {
+		padWithSpacesAround?: boolean;
+		padWithSpacesBetween?: boolean;
+	}
+}
+
+/** @public */
+class Concord extends Entity {
+	private static readonly CODE_PREFIX = "[";
+	private static readonly CODE_POSTFIX = "]";
+
+	/** Sorted from lowest to highest */
+	public readonly pitches: Pitch[] = [];
+
+	constructor(
+		public readonly duration: Duration,
+		items: (Note | Pitch)[]
+	) {
+		super();
+
+		this.pitches = items
+			.map((item) => "pitch" in item ? item.pitch : item)
+			.sort((a, b) => a.frequency - b.frequency);
+	}
+
+	getCode(params: Concord.GetCodeParams = {}): string {
+		const {
+			concise = true,
+			padWithSpacesAround = true,
+			padWithSpacesBetween = true,
+		} = params;
+
+		const pitchGetCodeParams = { concise };
+		const pitchCodes = this.pitches
+			.map((pitch) => pitch.getCode(pitchGetCodeParams))
+			.join(padWithSpacesBetween ? ", " : ",");
+
+		const spaceAround = padWithSpacesAround ? " " : "";
+
+		const chunks: string[] = [
+			Concord.CODE_PREFIX,
+			spaceAround,
+			...pitchCodes,
+			spaceAround,
+			Concord.CODE_POSTFIX,
+		];
+
+		return chunks.join("");
+	}
+}
+
+export default Concord;
