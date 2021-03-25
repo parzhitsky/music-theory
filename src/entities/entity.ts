b5befd6ad6fda0216726ec0b04a4a341597999f0
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-03-25 17:00:37 +0200

    Define Entity.Collection

--- src/entities/entity.ts
+++ src/entities/entity.ts
@@ -1,16 +1,43 @@
 /** @public */
-namespace Entity {
-	export interface GetCodeParams {
-		concise?: boolean;
-	}
-}
-
-/** @public */
 abstract class Entity {
 	// abstract equals(entity: this): boolean;
 
 	getCode(): string {
 		return `[object ${this.constructor.name}]`;
+	}
+}
+
+/** @public */
+namespace Entity {
+	export interface GetCodeParams {
+		concise?: boolean;
+	}
+
+	export abstract class Collection<Item extends Entity> extends Entity {
+		protected abstract readonly items: Item[];
+
+		get length(): number {
+			return this.items.length;
+		}
+
+		hasItem(index: number): boolean {
+			return this.items[index] != null;
+		}
+
+		getItem(index: number): Item {
+			if (!this.hasItem(index))
+				throw new Collection.ItemNotFoundError(index);
+
+			return this.items[index];
+		}
+	}
+
+	export namespace Collection {
+		export class ItemNotFoundError extends Error {
+			constructor(index: number) {
+				super(`Item with index ${index} does not exist in the collection`);
+			}
+		}
 	}
 }
 
