15efb05756f574c51b85277a9c47ef45a8d9f891
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-03-29 00:56:07 +0300

    Inherit Note from Entity.Adjustable

--- src/entities/note.ts
+++ src/entities/note.ts
@@ -2,9 +2,12 @@ import Entity from "./entity";
 import type Pitch from "./pitch";
 import type Duration from "./duration";
 import type Interval from "./interval";
+import type Adjustment from "./adjustment";
 
 /** @public */
-class Note extends Entity implements Entity.Transposable {
+class Note extends Entity implements Entity.Transposable, Entity.Adjustable {
+	public readonly adjustment = this.pitch.adjustment;
+
 	constructor(
 		public readonly duration: Duration,
 		public readonly pitch: Pitch,
@@ -14,6 +17,20 @@ class Note extends Entity implements Entity.Transposable {
 
 	transpose(interval: Interval, direction = Entity.Direction.up): Note {
 		return new Note(this.duration, this.pitch.transpose(interval, direction));
+	}
+
+	adjust(adjustment: Adjustment): Note {
+		if (this.adjustment.isZero)
+			return this;
+
+		return new Note(this.duration, this.pitch.adjust(adjustment));
+	}
+
+	unadjusted(): Note {
+		if (this.adjustment.isZero)
+			return this;
+
+		return new Note(this.duration, this.pitch.unadjusted());
 	}
 }
 
