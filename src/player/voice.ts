fe7afc563ad903de2d84249ca2b9e75b8163265c
Dmytro Parzhytskyi <dmytro_parzhytskyi@epam.com>
2021-04-07 13:38:23 +0300

    [WIP] Define stub for Voice

---
+++ src/player/voice.ts
@@ -0,0 +1,12 @@
+/** @public */
+class Voice {
+	public static readonly MAX_VOLUME = 127;
+
+	// TODO: play(note: Note, volume?: number): void
+}
+
+/** @public */
+namespace Voice {
+}
+
+export default Voice;
